45a29fb3a7ff22334bf76e2d803e65c929069785
Tenawerk <zelle.helena@gmail.com>
2023-06-15 17:51:21 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#includes<stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
