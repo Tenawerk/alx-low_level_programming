5a2ca4186d372dfcd8602fffd549e9346251143e
Tenawerk <zelle.helena@gmail.com>
2023-06-21 16:59:56 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet_x10 - Make alphabet x10 times
- * Return: void 
+ * Return: void
  */
 
 void print_alphabet_x10(void)
@@ -22,4 +22,3 @@ void print_alphabet_x10(void)
 	i++;
 	}
 }
-	
