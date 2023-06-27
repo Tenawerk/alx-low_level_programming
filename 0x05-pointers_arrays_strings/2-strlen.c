1c39bdd0a2cf1621884fee13e8ca539b345e495b
Tenawerk <zelle.helena@gmail.com>
2023-06-26 17:04:20 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.>
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the lenth of
+ * Return: The length of @str.
+ */
+size_t _strlen(const char *str)
+
+{
+	size_t lenth = 0;
+
+	while (*str++)
+	length++;
+	return (length);
+}
