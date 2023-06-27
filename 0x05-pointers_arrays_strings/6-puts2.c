2dc3de865d7e2daf8900f58ac063614ae120e99d
Tenawerk <zelle.helena@gmail.com>
2023-06-26 18:07:38 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other charcter of a string 
+ * puts2 - Prints every other charcter of a string
  * @str: The string to be treated
  * Return: void
  */
