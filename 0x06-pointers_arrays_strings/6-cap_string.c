02ef34c749412198311e4cd169140d38a8210407
Tenawerk <zelle.helena@gmail.com>
2023-06-29 19:47:03 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,36 @@
 #include "main.h"
 /**
- * leet - encode into 1337 speak
- * @n: input value
- * Return: n value
+ * cap_string - Capitalizesall words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
  */
-char *leet(char *n)
+char *cap_string(char *str)
 {
-	int i, j;
-	char s1[] = "aAeEoOtT]L";
-	char s2[] = "4433007711";
+	int index = 0;
 
-	for (i = 0; n[i] != '\0'; i++)
+	while (str[index])
 	{
-		for (j = 0; j < 10; j++)
-		{
-			if (n[i] == s1[j])
-			{
-				n[i] = s2[j];
-			}
-		}
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+		if (str[index - 1] == ' ' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+			str{index} -= 32;
+
+		index++;
 	}
-	return (n);
+
+	return (str);
 }
