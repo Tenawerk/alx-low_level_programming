317981d124b5772a286472821a527d58f24d27ce
Tenawerk <zelle.helena@gmail.com>
2023-07-06 16:55:04 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,16 @@
 #include "main.h"
-
 /**
- * _puts_recursion - Print a string followed by a new line
- * @s: the string to print
- * Return: Nothing.
+ * _puts_recursion - function like pts();
+ * @s: input
+ * Return: Always 0 (Success)
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-	}
-	else
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\n');
 }
