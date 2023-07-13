a3fe16423363428c1f14a9de03c4979b01e7582e
Tenawerk <zelle.helena@gmail.com>
2023-07-12 19:55:38 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * free_grig - ...
+ * @grid: ...
+ * @height: ...
+ *
+ * Return: ...
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
